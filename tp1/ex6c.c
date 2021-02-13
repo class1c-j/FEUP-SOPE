fffd3385d26c5d188f1513fa04e83d1d34bb1489
eukia <ritam.mendes@gmail.com>
2021-02-13 15:18:35 +0000

    Add tp1 ex6

---
+++ tp1/ex6c.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  int f = open(argv[1], O_RDONLY);
+  if (f == -1) {
+    perror("error opening the file\n");
+  } else {
+    close(f);
+  }
+}
